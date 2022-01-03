bafbd5f43e8c78ba8dddd4ddfca267f901cb9c0c
ShukurDev <shukurdev02@gmail.com>
2022-01-04 01:34:38 +0500

    ok

--- MbMB/config/settings.py
+++ MbMB/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.pythonany']
+ALLOWED_HOSTS = ['shukurcrm.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
