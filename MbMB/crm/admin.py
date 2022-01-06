6d83f8dc9b305afb2c7e2aacb6c7b72c2b75988b
ShukurDev <shukurdev02@gmail.com>
2022-01-06 10:12:16 +0500

    -

--- MbMB/crm/admin.py
+++ MbMB/crm/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['name','phone','email']
+    list_display = ['name', 'phone', 'email']
 
-admin.site.register(Customer,CustomerAdmin)
+
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Product)
 admin.site.register(Tag)
 admin.site.register(Order)
-
